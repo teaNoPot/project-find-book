d11b0e6504b6cfbf7f762ebde227e20b0449b15c
Abdel <serghiinc@gmail.com>
2022-03-05 14:08:09 +0100

    [main] Added HexColor, Finished Simple SearchBarUI

--- searchbook/lib/home.dart
+++ searchbook/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'package:searchbook/bookAppTheme.dart';
 
 
@@ -29,7 +30,7 @@ class _HomePageState extends State<HomePage> {
                   height: MediaQuery.of(context).size.height,
                   child: Column(
                     children: <Widget>[
-                      //getSearchBarUI(),
+                      getSearchBarUI(),
                       getBooksUI(),
                       Flexible(
                         child: getPopularBooksUI()
@@ -45,7 +46,7 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  // TODO: getAppBarUI - 122-001
+  // DONE: getAppBarUI - 122-001
   Widget getAppBarUI() {
     return Padding(
       padding: const EdgeInsets.only(top: 9.0, left: 18, right: 18),
@@ -90,6 +91,77 @@ class _HomePageState extends State<HomePage> {
   }
 
   // TODO: getSearchBarUI - 122-002:
+  Widget getSearchBarUI() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 9.0, left: 18),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width * 0.75,
+            height: 64,
+            child: Padding(
+              padding: const EdgeInsets.only(top:8.0, bottom: 8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: HexColor('#F8FAFB'),
+                  borderRadius: const BorderRadius.only(
+                    bottomRight: Radius.circular(13.0),
+                    bottomLeft: Radius.circular(13.0),
+                    topLeft: Radius.circular(13.0),
+                    topRight: Radius.circular(13.0),
+                  ),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        padding: const EdgeInsets.only(left: 16, right: 16),
+                        child: TextFormField(
+                          style: TextStyle(
+                            fontFamily: 'WorkSans',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: BookAppTheme.nearlyBlue,
+                          ),
+                          keyboardType: TextInputType.text,
+                          decoration: InputDecoration(
+                            labelText: 'Tap to begin the search...',
+                            border: InputBorder.none,
+                            helperStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: HexColor('#B9BABC'),
+                            ),
+                            labelStyle: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                              letterSpacing: 0.2,
+                              color: HexColor('#B9BABC'),
+                            ),
+                          ),
+                          onEditingComplete: () {},
+                        )
+                      ),
+                    ),
+                    SizedBox(
+                      width: 60,
+                      height: 60,
+                      child: Icon(Icons.search, color: HexColor('#B9BABC')),
+                    )
+                  ],
+                ),
+              )
+            ),
+          ),
+          const Expanded(
+            child: SizedBox(),
+          )
+        ],
+      )
+    );
+  }
 
   // TODO: getBooksUI - 122-003
   Widget getBooksUI() {
