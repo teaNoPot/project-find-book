81c9e96c9a53ebda690be54d385f09aed4cea257
Abdel <serghiinc@gmail.com>
2022-03-05 13:56:21 +0100

    [main] Created a simple AppBarUI

--- searchbook/lib/home.dart
+++ searchbook/lib/home.dart
@@ -22,7 +22,7 @@ class _HomePageState extends State<HomePage> {
             SizedBox(
               height: MediaQuery.of(context).padding.top,
             ),
-            //getAppBarUI(),
+            getAppBarUI(),
             Expanded(
               child: SingleChildScrollView(
                 child: Container(
@@ -45,21 +45,69 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  // TODO: getAppBarUI - 122-001
+  Widget getAppBarUI() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 9.0, left: 18, right: 18),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Search your',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 14,
+                    letterSpacing: 0.2,
+                    color: BookAppTheme.grey
+                  ),
+                ),
+                Text(
+                  'Favorite books',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                      letterSpacing: 0.27,
+                      color: BookAppTheme.darkerText
+                  ),
+                ),
+              ],
+            )
+          ),
+          Container(
+            width: 60,
+            height: 60,
+            child: Image.asset('assets/diamond.png'),
+          )
+        ],
+      ),
+    );
+  }
+
+  // TODO: getSearchBarUI - 122-002:
+
+  // TODO: getBooksUI - 122-003
   Widget getBooksUI() {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         Padding(
-          padding: const EdgeInsets.only(top: 8.0, left: 18, right: 16),
-          child: Text(
-            ''
-          )
+            padding: const EdgeInsets.only(top: 8.0, left: 18, right: 16),
+            child: Text(
+                ''
+            )
         )
       ],
     );
   }
 
+  // TODO: getPopularBooksUI - 122-004
   Widget getPopularBooksUI() {
     return Padding(
       padding: const EdgeInsets.only(top: 8.0, left: 18, right: 16),
@@ -93,8 +141,6 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-
-
 }
 
 
